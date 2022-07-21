21e637fc04109f55b15caa8d833faa04925a6929
Thallapelly Sumanth <tsumanth@ariqt.com>
2022-07-21 15:15:13 +0530

    initial commit

---
+++ apps/remote/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfe1-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'remote';
+}
